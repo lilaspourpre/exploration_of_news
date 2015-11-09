6b80be8e16e1706f94d9ac40e6cdb9b87de73444
lilasporpre <irina.nikishina@mail.ru>
2015-11-10 02:12:20 +0400

    1) think how to improve join 2) check all

--- sql_functions.py
+++ sql_functions.py
@@ -1,12 +1,13 @@
+# -*- coding: utf-8 -*-
 import psycopg2
 import sys
 import xml.etree.ElementTree as ET
-class sql_functions():
+class SQL_functions():
     def __init__(self):
         self.con = None
         self.cur = None
 
-    def open_config(config):
+    def open_config(self, config):
         tree = ET.parse(config)
         root = tree.getroot()
         string = ""
@@ -17,7 +18,7 @@ class sql_functions():
     def connection_on(self):
         try:
             self.con = psycopg2.connect(self.open_config("config.xml"))
-            self.cur = self.con.curr()
+            self.cur = self.con.cursor()
             return 'success'
         except psycopg2.DatabaseError, e:
             if self.con:
@@ -63,10 +64,50 @@ class sql_functions():
                     choix+=i+" "
         else:
             choix=choice
-        first = "SELECT "+choix+" FROM "+
+        first = "SELECT "+choix+" FROM "+self.where(choice,args,kwargs)
         second = " WHERE "
         third = self.condition_for_select(list(args),kwargs)+" 1=1"
-        self.execution(first, second, third)
+        return first, second, third
+
+    def arguments(self, arg):
+        list_args = []
+        for i in range(len(arg)): #находим знаки препинания (учитывая варианты <> != >= <=) и захватываем строку
+                if arg[i]=='<' or arg[i]=='>' or arg[i]=='=':
+                    if '=' in arg[:i-1] or '>' in arg[:i-1] or '<' in arg[:i-1]: #если условий много, как например (clustid<>0 AND textid=1)
+                        spaces = []
+                        for smth in range(len(arg[:i-1])):
+                            if arg[:i-1][smth] == " ":
+                                spaces.append(smth)
+                        if arg[i-1]!='<' and arg[i-1]!='>' and arg[i-1]!='=':
+                            list_args.append(arg[max(spaces):i])
+
+                    else: #если оно от начала и в одной строке не много условий
+                        if arg[i-1]!='<' and arg[i-1]!='>' and arg[i-1]!='=':
+                            list_args.append(arg[:i])
+        return list_args
+
+    def where(self, choice, args, kwargs):
+        where = []
+        for i in kwargs: #добавили из словаря названия переменных
+            choice.append(i)
+        for arg in args:
+            choice.extend(self.arguments(arg))
+        tables = self.where_search()
+        for column in choice:
+            for table in tables:
+                if column in tables[table]:
+                    where.append(table)
+        where = list(set(where))
+        if len(where) == 1:
+            return where[0]
+        join_string=''
+        for i in where:
+            if where.index(i)!=len(where)-1:
+                join_string+=i+" NATURAL JOIN "
+            else:
+                join_string+=i
+        return join_string
+        #подумать, как формировать join
 
     def condition_for_select(self, *args):
         condition = ""
@@ -93,6 +134,7 @@ class sql_functions():
         return condition
 
     def where_search(self):
+        self.connection_on()
         tables = {}
         self.cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';")
         for row in self.cur.fetchall():
@@ -101,11 +143,12 @@ class sql_functions():
             for r in self.cur.fetchall():
                     tab.append(r[0])
             tables[row[0]]=tab
+        self.connection_off()
         return tables
 
     def combine_all(self):
         self.connection_on()
         self.con.commit()
 
-
-
+a= SQL_functions()
+print a.select(["clustid", 'persid'],"persid>2 OR persid>2", textid=[2,3], persname='Bob')
